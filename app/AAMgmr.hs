23dd775bc36867d8f036423716a6c2036bc43ec7
xsj3n <xsj3n@tutanota.com>
2024-10-09 07:27:24 -0700

    push

--- app/AAMgmr.hs
+++ app/AAMgmr.hs
@@ -3,32 +3,25 @@ import System.IO.Strict as SI
 import Data.List as LI
 import Data.Bool
 import Data.Map as MP
+import Data.Text as TT
 
+qdelim :: Text 
+qdelim = TT.pack "\""
 
 x -: f = f x
 
 
-getIPKeyList :: IO (Map String String)
-getIPKeyList = SI.readFile "/home/xs/src/mdm/testnix.nix" >>=  return . parsePeers .findInnerOf "peers" "];" 
-
--- Returns what is between x and y in z string
-findInnerOf :: String -> String -> String -> [String]
-findInnerOf _ _ []  = []
-findInnerOf bound_one bound_two str = let lns = lines str
-                                          pred_one x = not $ isInfixOf bound_one x 
-                                          pred_two x = isInfixOf bound_two x
-                                      in  let half = (dropWhile (pred_one) lns) 
-                                          in  fst $ LI.splitAt (last (findIndices pred_two half)) half
+getIPKeyList :: IO (Map Text Text)
+getIPKeyList = SI.readFile "/home/xs/src/mdm/testnix.nix" >>=  return . parsePeers
 
 
-stripStrs :: [String] -> [String]
-stripStrs [] = []
-stripStrs (x:xs) = dropWhile (not == )
 
-parsePeers :: [String] -> Map String String
-parsePeers [] = MP.empty
-parsePeers xs = let indices = findIndices (isInfixOf "publicKey") xs
-                in MP.fromList $ LI.foldl (\acc x -> [((xs !! x), (xs!! (x + 1)))] ++ acc) [] indices
+parsePeers :: String -> Map Text Text 
+parsePeers txt = let lns = TT.lines $ TT.pack txt 
+                 in  let indices = findIndices (TT.isInfixOf $ TT.pack "publicKey") lns 
+                     in  MP.fromList 
+                         $ LI.map (\x -> (splitOn qdelim x)  !! 1 )
+                         $ LI.foldl (\acc x -> acc ++ [(lns !! x, lns !! x + 1)]) [] indices
 
 
 
